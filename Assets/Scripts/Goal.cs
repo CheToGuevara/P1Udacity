52e5b51f8da13606a7cb5b1d74e3cf369be633a8
Aaron Sujar <Aaron Sujar>
2016-11-02 15:14:43 +0100

    First Commit

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour
+{
+
+  public Color mycolor;
+  public GameObject mlight;
+  public GameObject sphere;
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    mlight.GetComponent<Renderer>().material.color = mycolor;
+    mlight.GetComponent<Light>().enabled = true;
+
+  }
+
+
+  public void resetSphere()
+  {
+    sphere.transform.localPosition = new Vector3(3.5f, 14.75f, 7);
+    sphere.transform.localRotation = new Quaternion(0, 0, 0,1);
+    sphere.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+    sphere.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# P1Udacity
-Project 1 of Udacity VR Nanodegrre
+Aaron Sujar
+
+It takes me around 8 hours in my weekend. 
+One thing I like is that is my first time to complete a VR app, because I tried before but I didn't finished it.
+For me, the most challenging thing is to prepare the project in order to send to Udacity platform
